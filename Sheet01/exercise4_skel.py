8baedd2b02c2acc64e61958ffde606411f0bf4e7
Jens-Peter M. Zemke <zemke@tuhh.de>
2023-10-10 15:48:01 +0200

    Skeleton for exercise 4 of sheet 1

---
+++ Sheet01/exercise4_skel.py
@@ -0,0 +1,22 @@
+import numpy as np 
+
+"""Skeleton for exercise 4 of exercise sheet 1"""
+
+class ReLU():
+
+    """Implementation of the ReLU activation function."""
+
+    ### add here your implementation of the class ReLU
+
+if __name__ == '__main__':
+    
+    fun = ReLU()
+    x = np.arange(-5, 6, dtype=float)
+
+    print(x)
+    print(fun(x))
+    """
+    Expected output:
+    >>> [-5. -4. -3. -2. -1.  0.  1.  2.  3.  4.  5.]
+    >>> [0. 0. 0. 0. 0. 0. 1. 2. 3. 4. 5.]  
+    """
